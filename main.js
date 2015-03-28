17e66e28995c265d68eebdeb18b3d93ba3e2eff9
Demos <demo@sm.local>
2015-03-28 16:17:04 -0700

    Game markup

--- main.js
+++ main.js
@@ -1,64 +1,13 @@
-var CommentsBox = React.createClass({
-  getInitialState: function() {
-    return ({
-      data: ["This is the first comment", "This is the second comment"],
-      currentComment: ''
-   });
-  },
-  changeData: function(event) {
-    console.log("changeData function", event.target.value);
-    this.setState({currentComment: event.target.value});
-    return false;
-  },
-  handleSubmit: function(event) {
-    var currentData = this.state.data;
-    // var newData = currentData;
-    // newData.push(this.state.currentComment);
-    var newData = currentData.concat([this.state.currentComment]);
-    var newComment = '';
-    this.setState({currentComment: newComment, data: newData});
-    return false;
-  },
+var Game = React.createClass({
   render: function() {
     return (
-      <div className="">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.currentComment} onChange={this.changeData} />
-          <button type="submit">Send</button>
-        </form>
-
-        <CommentsList data={this.state.data} />
-      </div>
-    );
-  }
-});
-
-var CommentsList = React.createClass({
-  render: function() {
-    var content = this.props.data.map(function(element, i) {
-      return (
-        <SingleComment commentText={element} key={i}/>
-      );
-    });
-    return (
-      <div id="commentsList">
-        {content}
-      </div>
-    );
-  }
-});
-
-var SingleComment = React.createClass({
-  render: function() {
-    return (
-      <div className="one-comment">
-        {this.props.commentText}
+      <div className="game-frame">
       </div>
     );
   }
 });
 
 React.render(
-  <CommentsBox />,
+  <Game />,
   document.getElementById('main')
 );
