3cd66c631dedd28f9bbc83c5886ee6ab5857e037
Demos <demo@sm.local>
2015-03-28 16:50:37 -0700

    Draw stars

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 var Game = React.createClass({
+  getInitialState: function() {
+    return {currentNumberOfStars: 0};
+  },
+  generateRandomNumber: function() {
+    return Math.floor(Math.random() * 11 + 2);
+  },
+  drawStars: function() {
+    this.setState({currentNumberOfStars: this.generateRandomNumber()});
+  },
   render: function() {
     return (
       <div className="game-frame">
         <div id="upper" className="row">
           <div id="stars" className="col-sm-5">
             <div className="content">
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
+              <StarsFrame currentNumberOfStars={this.state.currentNumberOfStars} />
             </div>
           </div>
           <div id="equal" className="col-sm-2">
@@ -19,7 +25,7 @@ var Game = React.createClass({
         </div>
         <div id="button" className="row">
           <div className="col-sm-4 col-sm-offset-4">
-            <button type="button" className="btn btn-default btn-wide">Draw</button>
+            <button type="button" className="btn btn-default btn-wide" onClick={this.drawStars} >Draw</button>
           </div>
         </div>
         <div id="lower" className="row">
@@ -29,6 +35,23 @@ var Game = React.createClass({
             <span className="number">3</span>
           </div>
         </div>
+      </div>
+    );
+  }
+});
+
+var StarsFrame = React.createClass({
+  render: function() {
+    // var numberOfStars = 7;
+    var content = [];
+
+    for (var i=0; i < this.props.currentNumberOfStars; i++) {
+      content.push(<i className="fa fa-2x fa-star"></i>);
+    }
+
+    return (
+      <div>
+        {content}
       </div>
     );
   }
